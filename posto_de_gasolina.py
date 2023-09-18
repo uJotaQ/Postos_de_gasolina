bcf32b84e5a22b349bccfab74a77d3cc6f6a32ab
João Gabriel Santos Silva <145305652+uJotaQ@users.noreply.github.com>
2023-09-18 12:25:21 -0300

    Update posto_de_gasolina.py

--- posto_de_gasolina.py
+++ posto_de_gasolina.py
@@ -7,7 +7,7 @@ Componente Curricular: Algoritmos I
 Concluido em: 28/08/2023
 Declaro que este código foi elaborado por mim de forma
 individual e não contém nenhum trecho de código de outro
- colega ou de outro autor, tais como provindos de livros e
+colega ou de outro autor, tais como provindos de livros e
 apostilas, e páginas ou documentos eletrônicos da Internet.
 Qualquer trecho de código de outra autoria que não a minha
 está destacado com uma citação para o autor e a fonte do código,
